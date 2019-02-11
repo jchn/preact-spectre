e0b1018711aa2ba872b47bfa88dccb3a18c0e570
jchn <john.vd.water85@gmail.com>
2019-02-11 20:56:41 +0100

    configure hierarchyRootSeparator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
-import { configure } from '@storybook/react'
+import { addDecorator, configure } from '@storybook/react'
+import { withOptions } from '@storybook/addon-options'
+
+addDecorator(
+  withOptions({
+    hierarchyRootSeparator: /\|/,
+  })
+)
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/)
+const req = require.context('../src/components', true, /.stories.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
