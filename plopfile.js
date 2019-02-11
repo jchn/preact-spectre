5daba9f1772b70723a8a7d1c88b5712ebfc0f571
jchn <john.vd.water85@gmail.com>
2019-02-11 20:55:50 +0100

    Add plopfile and templates

---
+++ plopfile.js
@@ -0,0 +1,37 @@
+module.exports = function(plop) {
+  plop.setGenerator('component', {
+    description: 'component',
+
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name?',
+      },
+      {
+        type: 'input',
+        name: 'className',
+        message: 'Class name?',
+      },
+    ],
+
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.js',
+        templateFile: 'templates/component/component.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.js',
+        templateFile: 'templates/component/index.hbs',
+      },
+      {
+        type: 'add',
+        path:
+          'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.js',
+        templateFile: 'templates/component/component.stories.hbs',
+      },
+    ],
+  })
+}
