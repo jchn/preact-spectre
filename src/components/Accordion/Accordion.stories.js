d86add45aec5436eade70b2ef5436db515b91ae7
jchn <john.vd.water85@gmail.com>
2019-02-18 23:07:49 +0100

    Update story

--- src/components/Accordion/Accordion.stories.js
+++ src/components/Accordion/Accordion.stories.js
@@ -3,10 +3,25 @@ import centered from '@storybook/addon-centered'
 import { storiesOf } from '@storybook/react'
 import Accordion from './'
 
-storiesOf('Components|Accordion', module).add('Default', () => (
-  <Accordion exclusive arrow>
-    <Accordion.Item title="Elements">Element 1</Accordion.Item>
-    <Accordion.Item title="Layouts">Layout 1</Accordion.Item>
-    <Accordion.Item title="Components">Component 1</Accordion.Item>
-  </Accordion>
-))
+storiesOf('Components|Accordion', module)
+  .add('Default', () => (
+    <Accordion>
+      <Accordion.Item title="Elements">Element 1</Accordion.Item>
+      <Accordion.Item title="Layouts">Layout 1</Accordion.Item>
+      <Accordion.Item title="Components">Component 1</Accordion.Item>
+    </Accordion>
+  ))
+  .add('exclusive', () => (
+    <Accordion exclusive>
+      <Accordion.Item title="Elements">Element 1</Accordion.Item>
+      <Accordion.Item title="Layouts">Layout 1</Accordion.Item>
+      <Accordion.Item title="Components">Component 1</Accordion.Item>
+    </Accordion>
+  ))
+  .add('arrow', () => (
+    <Accordion arrow>
+      <Accordion.Item title="Elements">Element 1</Accordion.Item>
+      <Accordion.Item title="Layouts">Layout 1</Accordion.Item>
+      <Accordion.Item title="Components">Component 1</Accordion.Item>
+    </Accordion>
+  ))
