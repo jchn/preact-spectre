0535daf3351a93007345b9537e7217c89947827a
jchn <john.vd.water85@gmail.com>
2019-02-20 00:23:07 +0100

    Import all stories within src

--- .storybook/config.js
+++ .storybook/config.js
@@ -8,7 +8,7 @@ addDecorator(
 )
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/components', true, /.stories.js$/)
+const req = require.context('../src', true, /.stories.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
