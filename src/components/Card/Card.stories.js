a9aa13d27ff6e0847798ef371e8a279d419a2cae
jchn <john.vd.water85@gmail.com>
2019-02-20 20:27:34 +0100

    Update story

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -2,28 +2,28 @@ import { h } from 'preact'
 import centered from '@storybook/addon-centered'
 import { storiesOf } from '@storybook/react'
 import Card from './'
+import Button from '../Button'
+import Grid from '../Grid'
 import faker from 'faker'
 
 storiesOf('Components|Card', module)
   .addDecorator(centered)
   .add('Default', () => (
-    <Card>
-      <Card.Header>
-        <Card.Title>Card title</Card.Title>
-        <Card.Subtitle>Card subtitle</Card.Subtitle>
-      </Card.Header>
-      <Card.Body>Card body</Card.Body>
-      <Card.Footer>Card footer</Card.Footer>
-    </Card>
-  ))
-  .add('Image', () => (
-    <Card>
-      <Card.Image src={faker.image.nature()} alt="card image" />
-      <Card.Header>
-        <Card.Title>Card title</Card.Title>
-        <Card.Subtitle>Card subtitle</Card.Subtitle>
-      </Card.Header>
-      <Card.Body>Card body</Card.Body>
-      <Card.Footer>Card footer</Card.Footer>
-    </Card>
+    <Grid size="md">
+      <Grid.Columns>
+        <Grid.Col col={6} class="col-mx-auto">
+          <Card>
+            <Card.Image src={faker.image.nature()} alt="card image" />
+            <Card.Header>
+              <Card.Title class="h5">Card title</Card.Title>
+              <Card.Subtitle class="text-gray">Card subtitle</Card.Subtitle>
+            </Card.Header>
+            <Card.Body>{faker.lorem.paragraph()}</Card.Body>
+            <Card.Footer>
+              <Button intent="primary">Save</Button>
+            </Card.Footer>
+          </Card>
+        </Grid.Col>
+      </Grid.Columns>
+    </Grid>
   ))
